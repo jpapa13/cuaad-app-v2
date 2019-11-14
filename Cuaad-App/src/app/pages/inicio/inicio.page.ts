6edefc9ab2bce528e196d8ce2903f006d4b90c41
Juan Pablo Medina Quirarte <juan.mquirarte@alumnos.udg.mx>
2019-11-14 16:47:56 -0600

    Agrega Artes y Música a los banners del inicio

--- Cuaad-App/src/app/pages/inicio/inicio.page.ts
+++ Cuaad-App/src/app/pages/inicio/inicio.page.ts
@@ -16,25 +16,61 @@ export class InicioPage {
 
  public archivo: any [];
  public galeriaHuenti: Galeria;
- public  url = 'http://localhost/Cuaad-App/cuaad-app-v2/BaseBackend/index.php/';
+ public galeriaMusica: Galeria;
+ public galeriaArtes: Galeria;
+ public galeria: Galeria;
+// public  url = '192.168.64.2/cuaad-app-v2/BaseBackend/index.php/';
 
   constructor( private request: RequestService,
                private http: HTTP,
                private modalCtrl: ModalController ) {
-
+              this.galeriaHuenti = new Galeria ( 'Huentitan', 'imagen' );
+              this.galeriaMusica = new Galeria ( 'Música', 'imagen' );
+              this.galeriaArtes = new Galeria ( 'Artes', 'imagen' );
+              this.galeria = new Galeria('Todos', 'imagen');
               this.getAllBanners();
   }
 
   getAllBanners() {
-    this.galeriaHuenti = new Galeria ( 'Huentitan', 'imagen' );
     this.request.getBanners(this.galeriaHuenti)
     .subscribe((Response: any) => {
 
     console.log(Response);
     if (Response.status === true) {
-        console.log('Respuesta satisfactoria');
+        console.log('Respuesta satisfactoria Huenti');
         this.galeriaHuenti.archivo = Response.data;
+        this.galeria.archivo = this.galeria.archivo.concat(this.galeriaHuenti.archivo);
         console.log(this.galeriaHuenti.archivo);
+      } else {
+        console.log('Error:');
+        console.log(Response);
+      }
+    });
+
+    this.request.getBanners(this.galeriaMusica)
+    .subscribe((Response: any) => {
+
+    console.log(Response);
+    if (Response.status === true) {
+        console.log('Respuesta satisfactoria Música');
+        this.galeriaMusica.archivo = Response.data;
+        this.galeria.archivo = this.galeria.archivo.concat(this.galeriaMusica.archivo);
+        console.log(this.galeriaMusica.archivo);
+      } else {
+        console.log('Error:');
+        console.log(Response);
+      }
+    });
+
+    this.request.getBanners(this.galeriaArtes)
+    .subscribe((Response: any) => {
+
+    console.log(Response);
+    if (Response.status === true) {
+        console.log('Respuesta satisfactoria Artes');
+        this.galeriaArtes.archivo = Response.data;
+        this.galeria.archivo = this.galeria.archivo.concat(this.galeriaArtes.archivo);
+        console.log(this.galeriaArtes.archivo);
       } else {
         console.log('Error:');
         console.log(Response);
