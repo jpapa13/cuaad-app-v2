f22d8481b4807dc8ac6e2a1a6dcb9bc32a327516
Juan Pablo Medina <juan@ayudatelta.com>
2020-04-14 16:44:38 -0500

    Actualiza ids en Horarios Frontend

--- Cuaad-App/src/app/pages/lugares/lugares.page.ts
+++ Cuaad-App/src/app/pages/lugares/lugares.page.ts
@@ -14,8 +14,7 @@ export class LugaresPage{
   edificio:any;
   vacio = {profesor:'Vacio'}
   constructor(private sHorarios: HorariosService,
-              private request: RequestService) {
-  }
+              private request: RequestService) {}
 
   change(){
     this.sHorarios.getAulasEdificio(this.edificio);
@@ -28,9 +27,25 @@ export class LugaresPage{
       if(event.container.data == this.sHorarios.profesAsignados){ //Es en la primer lista
         const detalle_1 = this.sHorarios.profesAsignados[event.currentIndex].detalle_id
         const detalle_2 = this.sHorarios.profesAsignados[event.previousIndex].detalle_id
-        this.request.intercambiarHorario(detalle_1,detalle_2).subscribe((Response: any) => {
+        console.log(detalle_1)//profesor Vacio
+        console.log(detalle_2)
+        if(detalle_1 == 'null'){
+          console.log('Por vacio')
+          const aula = this.sHorarios.aulasAsignadas[event.currentIndex] //aula
+          console.log(aula)
+          const edificioNombre = this.sHorarios.edificio.lista[this.edificio][1][0].edificio
+          console.log(edificioNombre)
+          this.request.asignarHorario(detalle_2, aula, edificioNombre).subscribe((Response: any) => {
             console.log(Response);
           });
+        }else{
+          this.request.intercambiarHorario(detalle_1,detalle_2).subscribe((Response: any) => {
+            console.log(Response);
+            this.sHorarios.profesAsignados[event.currentIndex].detalle_id = Response.data.detalle2.id;
+            this.sHorarios.profesAsignados[event.previousIndex].detalle_id = Response.data.detalle1.id;
+          });
+          //console.log('No vacio')
+        }
       }
       moveItemInArray(
         event.container.data,
@@ -63,33 +78,30 @@ export class LugaresPage{
     } else { //intercambia con otra lista       
       if (event.previousContainer.data == this.sHorarios.profesAsignados) { //Del primero al segundo
         console.log('primero')
-        if (event.currentIndex != 0) { //Intercambia
+        if (event.currentIndex != 0) { // NO Intercambia
           transferArrayItem(event.previousContainer.data,
             event.container.data,
             event.previousIndex,
             event.currentIndex
           );
-          transferArrayItem(event.container.data,
-            event.previousContainer.data,
-            event.currentIndex + 1,
-            event.previousIndex
-          );
-        }else{ //Saca al profe
+         }else{ //Saca al profe
           console.log(event.container.data)
           console.log(this.sHorarios.profesAsignados)
-          console.log(event.previousContainer.data.profesor)
+          //console.log(event.previousContainer.data.profesor)
           transferArrayItem(event.previousContainer.data,
             event.container.data,
             event.previousIndex,
             event.currentIndex+1
           );
-          this.sHorarios.profesAsignados.splice(event.previousIndex,0,this.vacio)
+          
           //console.log(this.sHorarios.profesOtros[event.currentIndex+1])
-          var body = {detalle_id:this.sHorarios.profesOtros[event.currentIndex+1].detalle_id}
-          this.request.borrarHorario(body).subscribe((Response: any) => {
-            console.log(Response);
-          });
+          var body = {detalle_id:this.sHorarios.profesOtros[event.currentIndex+1].detalle_id}          
         }
+        this.request.borrarHorario(body).subscribe((Response: any) => {
+          console.log(Response.data.id);
+          this.sHorarios.profesOtros[event.currentIndex+1].detalle_id = Response.data.id
+        });
+        this.sHorarios.profesAsignados.splice(event.previousIndex,0,this.vacio)
       }else{ // del segundo al primero
         console.log('segundo')
 
