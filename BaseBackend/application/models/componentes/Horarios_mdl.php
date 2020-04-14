0d1991d7324b152966a33c66150483fb03aadb0d
Juan Pablo Medina <juan@ayudatelta.com>
2020-04-14 16:01:38 -0500

    Regresa nuevo id en Horarios Backend

--- BaseBackend/application/models/componentes/Horarios_mdl.php
+++ BaseBackend/application/models/componentes/Horarios_mdl.php
@@ -8,7 +8,7 @@ class Horarios_mdl extends CI_Model{
     }
     public function todos()
     {   
-        /*SELECT detalle_siiau.aula_fk_nombre AS nombre, profesor.nombre AS profesor, detalle_siiau.aula_fk_sede AS edificio FROM `detalle_siiau`
+        /*SELECT detalle_siiau.aula_fk_nombre AS nombre, profesor.nombre AS profesor, detalle_siiau.aula_fk_sede AS edificio FROM `detalle_siiau`
             JOIN siiau ON detalle_siiau.siiau_fk = siiau.id
             JOIN profesor ON siiau.profesor_fk = profesor.id
             WHERE `hora_ini` < CURRENT_TIME AND hora_fin > CURRENT_TIME AND dias LIKE '%J%' ORDER BY nombre*/
@@ -53,6 +53,7 @@ class Horarios_mdl extends CI_Model{
     { 
         //UPDATE `detalle_siiau` SET `activo` = '0' WHERE `detalle_siiau`.`id` = 1
         //INSERT INTO `detalle_siiau` (`id`, `hora_ini`, `hora_fin`, `dias`, `aula_fk_nombre`, `activo`, `siiau_fk`, `actualizacion`, `aula_fk_sede`) VALUES (NULL, '14:00:00', '17:55:00', 'I', NULL, '1', '503', CURRENT_TIMESTAMP, NULL);
+        //SELECT * FROM `detalle_siiau` ORDER BY `dias`  DESC
         $this->db->trans_start();
 
         $data = [
@@ -71,14 +72,22 @@ class Horarios_mdl extends CI_Model{
         $result = $this->db->get()->result_array();
 
         $sql = $this->db->insert('detalle_siiau',$result[0]);
+
+        $this->db->from('detalle_siiau');
+        $this->db->order_by('id','DESC');
+        $this->db->limit('1');
+        $this->db->select('id');
+        $result = $this->db->get()->result();
         $this->db->trans_complete();
-        return $sql;
+        return $result[0];
     }
 
     public function asignar_detalle($detalle_id, $edificio, $aula)
     { 
-        //UPDATE `detalle_siiau` SET `activo` = '0' WHERE `detalle_siiau`.`id` = 1
+        //UPDATE `detalle_siiau` SET `activo` = '0' WHERE `detalle_siiau`.`id` = $detalle_id
         //INSERT INTO `detalle_siiau` (`id`, `hora_ini`, `hora_fin`, `dias`, `aula_fk_nombre`, `activo`, `siiau_fk`, `actualizacion`, `aula_fk_sede`) VALUES (NULL, '14:00:00', '17:55:00', 'I', NULL, '1', '503', CURRENT_TIMESTAMP, NULL);
+       //SELECT * FROM `detalle_siiau` ORDER BY `dias`  DESC
+
         $this->db->trans_start();
 
         $data = [
@@ -99,14 +108,25 @@ class Horarios_mdl extends CI_Model{
         $result['aula_fk_nombre'] = $aula;
         $result['aula_fk_sede'] = $edificio;
         $sql = $this->db->insert('detalle_siiau',$result);
+
+        // New id
+        $this->db->from('detalle_siiau');
+        $this->db->order_by('id','DESC');
+        $this->db->limit('1');
+        $this->db->select('id');
+        $result = $this->db->get()->result();
+
         $this->db->trans_complete();
-        return $this->db->trans_status();
+        return $result[0];
     }
 
     public function intercambiar_detalle($detalle_1, $detalle_2)
     { 
         //UPDATE `detalle_siiau` SET `activo` = '0' WHERE `detalle_siiau`.`id` = 1 OR `detalle_siiau`.`id` = 2
         //INSERT INTO `detalle_siiau` (`id`, `hora_ini`, `hora_fin`, `dias`, `aula_fk_nombre`, `activo`, `siiau_fk`, `actualizacion`, `aula_fk_sede`) VALUES (NULL, '14:00:00', '17:55:00', 'I', NULL, '1', '503', CURRENT_TIMESTAMP, NULL);
+        //SELECT * FROM `detalle_siiau` ORDER BY `dias`  DESC
+        //INSERT INTO `detalle_siiau` (`id`, `hora_ini`, `hora_fin`, `dias`, `aula_fk_nombre`, `activo`, `siiau_fk`, `actualizacion`, `aula_fk_sede`) VALUES (NULL, '14:00:00', '17:55:00', 'I', NULL, '1', '503', CURRENT_TIMESTAMP, NULL);
+        //SELECT * FROM `detalle_siiau` ORDER BY `dias`  DESC
         $this->db->trans_start();
 
         $data = [
@@ -135,13 +155,26 @@ class Horarios_mdl extends CI_Model{
         
         //$sql = array('result1' => $result1, 'result2' => $result2);
         $sql1 = $this->db->insert('detalle_siiau',$result1[0]); 
-        $sql2 = $this->db->insert('detalle_siiau',$result2[0]); 
+        // New id 1
+        $this->db->from('detalle_siiau');
+        $this->db->order_by('id','DESC');
+        $this->db->limit('1');
+        $this->db->select('id');
+        $result1 = $this->db->get()->result();
 
+        $sql2 = $this->db->insert('detalle_siiau',$result2[0]); 
+        // New id 2
+        $this->db->from('detalle_siiau');
+        $this->db->order_by('id','DESC');
+        $this->db->limit('1');
+        $this->db->select('id');
+        $result2 = $this->db->get()->result();
 
 
         //$sql = $this->db->affected_rows();
         $this->db->trans_complete();
-        return $sql1.$sql2;
+        return array('detalle1' => $result1[0],
+                     'detalle2' => $result2[0]);
     }
 
     public function __destruct()
