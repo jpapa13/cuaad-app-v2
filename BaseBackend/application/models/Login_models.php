0b5a0e6eb14d9a3a1051f24ac89fdada6faef7da
Juan Pablo Medina <juan@ayudatelta.com>
2020-05-22 19:30:14 -0500

    Modifica modelo de login para manejar errores

--- BaseBackend/application/models/Login_models.php
+++ BaseBackend/application/models/Login_models.php
@@ -22,11 +22,15 @@ class Login_models extends CI_Model {
 
         ));
     $usuario = $this->db->get()->row();
-    $token = $usuario->id.' '.$usuario->usuario;
-    $token_encriptado = $this->encryption->encrypt($token);
-    $fecha_expiracion = modificacionFecha(date('Y-m-d H:i:s'),$this->config->item('sess_expiration'),'second','+');
-    $usuario->token = $token_encriptado;
-    $usuario->token_expiracion = $fecha_expiracion;
+    if(is_null($usuario)){
+        return false;
+    }else{
+        $token = $usuario->id.' '.$usuario->usuario;
+        $token_encriptado = $this->encryption->encrypt($token);
+        $fecha_expiracion = modificacionFecha(date('Y-m-d H:i:s'),$this->config->item('sess_expiration'),'second','+');
+        $usuario->token = $token_encriptado;
+        $usuario->token_expiracion = $fecha_expiracion;
+    }
     return $usuario; 
 
         
